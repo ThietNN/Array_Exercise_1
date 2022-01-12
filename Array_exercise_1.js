519d74fdc2208514bd757f14b46afa2ff248c7a3
Thiet Nguyen <nguyenthiet1225@gmail.com>
2022-01-12 10:59:26 +0700

    new pj

---
+++ Array_exercise_1.js
@@ -0,0 +1,9 @@
+let a = ["Red","Green","White","Black"];
+console.log(a.toString());
+console.log(a.join());
+console.log(a.join('+'))
+
+
+document.write(a.toString() + "<br>");
+document.write(a.join() + "<br>");
+document.write(a.join('+'));
